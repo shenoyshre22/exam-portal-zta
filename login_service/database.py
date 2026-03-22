2fd13d55a77e9fc241d6089c21a560e1a1dfe6a9
shenoyshre22 <shreyashenoy0222@gmail.com>
2026-03-22 23:15:42 +0530

    implement database connection and user table creation; also added empty auth and models files

---
+++ login_service/database.py
@@ -0,0 +1,19 @@
+import sqlite3  #python pre intalled , this function connects to our database file
+def get_db_connection(): #command helps us to open/connect to users.db
+    conn = sqlite3.connect('users.db')  #connects to the database file named users.db
+    conn.row_factory = sqlite3.Row  #this allows us to access the columns of the database by name instead of index like in tuples 
+    return conn  
+#------to create user table ---
+def create_users_table():
+    conn=get_db_connection() #connect to the database function we made above
+    #SQL query will now create table if it doesnt exist with username , password and role 
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,   -- unique username
+            password TEXT NOT NULL,      -- hashed password
+            role TEXT NOT NULL           -- student or teacher
+        )
+    """)
+    conn.commit() # save changes
+    conn.close() #this is imp to avoid database locks
+    
