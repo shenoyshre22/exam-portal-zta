8409fc5d96c4a771049334de6b05627db76e79ee
shenoyshre22 <shreyashenoy0222@gmail.com>
2026-03-22 23:32:06 +0530

    add loginreq model for API login data validation with string check

---
+++ login_service/models.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+# this defines what data your API expects for login
+class LoginRequest(BaseModel):
+    username: str   # to declare that itmust be a string
+    password: str   # must be a string
+    #using fast api here to check input format and throw error if theres any missing fields
+    
