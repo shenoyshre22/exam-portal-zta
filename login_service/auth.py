de4d49b99da8f6ce6ca47a17078dfbdefaa36fa7
shenoyshre22 <shreyashenoy0222@gmail.com>
2026-03-24 15:11:31 +0530

    set up the login page with password hashing , to confirm user if present in database, to check password

---
+++ login_service/auth.py
@@ -0,0 +1,27 @@
+from database import get_db_connection #to connect to our database
+from passlib.context import CryptContext #this inbuilt library will help us hash out passwords
+#the algorithm bcrpyt which is a hashing algorithm used in real systems
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") #this is the setup that will help us hash and verify passwords
+
+#---function to check if the entered password matches the stored password-----
+def verify_password(plain_password,hased_password):
+    #hashed password is the one stored in the database and plain password is the one entered by the user
+    return pwd_context.verify(plain_password,hased_password) #verify will help comparing safely
+ 
+ #---function to fetch a user from the database---
+ def get_user(username: str):
+    conn = get_db_connection() #open and connect to the database 
+    #sql query
+    user = conn.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone() #fetch the user with the given username
+    conn.close() #close the connection
+    return user #return the user data
+
+#---function to handle login logic ---
+def authenticate_user(username: str , password: str):
+    #checks if user exists and if password is correct
+    user=get_user(username) 
+    if not user:
+        return None
+    if not verify_password(password,user["password"]):
+        return None
+    return user #if both checks pass, then return the user data
