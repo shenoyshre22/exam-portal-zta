3e08906b4384b3179dfa8c7afb25ac7430468e3b
shenoyshre22 <shreyashenoy0222@gmail.com>
2026-03-24 19:44:12 +0530

    complete login microservice (so far) with user auth , signup service and verification of password

--- login_service/main.py
+++ login_service/main.py
@@ -28,4 +28,25 @@ def startup():
         #save changes and close the database connection
         conn.commit()
         conn.closer()
-        
+@app.get("/")
+def home():
+        return{"message": "Login Service Running yay"}  # just to test for our sake , if the service runs
+
+    #---signup API to create a new user--- 
+@app.post("/signup")
+def signup(data: SignupRequest): # takes in the inputs username , password and role (student/teacher)
+        if data.role not in ["student", "teacher"]:  # to avoid any other role but student or teacher
+            raise HTTPException(status_code=400, detail="Role must be either 'student' or 'teacher'") #if role is not valid , send error response
+        success = create_user(data.username, data.password, data.role) #create user using the function we defined in auth.py
+        if not success: # if user already exists and someone tries to sign in 
+            raise HTTPException(status_code=400, detail="Username already exists") #if user already exists , send error response
+        return {"message": "User created successfully"} #if user is created successfully , send success response 
+
+    
+#-- login API (to authenticate existing user and return their role)---
+@app.post("/login")
+def login(data: LoginRequest): #takes in username and password as input , validated and verifies
+    user = authenticate_user(data.username, data.password) #authenticate user using the function we defined in auth.py by calling it
+    if not user: #if authentication fails (likee if user doesnt exist or password is wrong)
+        raise HTTPException(status_code=401, detail="Invalid username or password") #send error response
+    return {"message": "Login successful", "role": user["role"]} #if login is successful , send success response with the user's role (student/teacher)
