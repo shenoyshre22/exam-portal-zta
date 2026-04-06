04d4e48640c72ec2fafe1be008e33ff1558821f8
Shreya <shreyamattimadu@gmail.com>
2026-04-06 12:09:24 +0530

    edited Frontend

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,24 +1,48 @@
 import { useState } from "react";
+import API from "../services/api";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+ const handleLogin = async () => {
+  try {
+    const formData = new URLSearchParams();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    const res = await API.post("/login", formData);
+
+    localStorage.setItem("token", res.data.access_token);
+    window.location.href = "/dashboard";
+
+  } catch (err) {
+    console.log(err.response);
+    alert("Login failed");
+  }
+};
+
+
   return (
     <div>
       <h1>Login Page</h1>
+
       <input
         placeholder="Username"
         onChange={(e) => setUsername(e.target.value)}
       />
+
       <br /><br />
+
       <input
         type="password"
         placeholder="Password"
         onChange={(e) => setPassword(e.target.value)}
       />
+
       <br /><br />
-      <button>Login</button>
+
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
